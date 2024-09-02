81e9ff5f32a4cd30c9882bcdc94944a0c8264e14
codesalotl <jaspertoribio10@gmail.com>
2024-09-02 22:12:56 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 // respec-app\app\page.tsx
 
-import ResultsPage from '@/app/components/results-page';
+import ResultsPage from "@/app/components/results-page";
 
 export default function Home() {
-    return (
-        <main>
-            <h1>Hello World</h1>
-            <br />
-            <ResultsPage />
-        </main>
-    );
+  return (
+    <main>
+      <h1>Hello World</h1>
+      <br />
+      <ResultsPage />
+    </main>
+  );
 }
